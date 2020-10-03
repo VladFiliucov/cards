c33daed3623e88f65e89cb608fe092e23164a614
Vlad Filiucov <filiucov@mail.ru>
2020-10-03 22:34:53 +0100

    add newDeck function

--- deck.go
+++ deck.go
@@ -6,6 +6,21 @@ import (
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	allSuits := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	allValues := []string{"Ace", "Deuce", "Jack", "Queen", "King"}
+
+	for _, suit := range allSuits {
+		for _, value := range allValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
