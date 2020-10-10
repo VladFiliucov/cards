38a31554d4e2e0be3dd9f82d6f5dff9eb76f092e
Vlad Filiucov <filiucov@mail.ru>
2020-10-10 16:48:20 +0100

    add function to read deck from file

--- main.go
+++ main.go
@@ -2,5 +2,8 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.saveToFile("cards.txt")
+	// cards.saveToFile("cards.txt")
+	// newDeckFromFile("cards.txt").print()
+	cards.shuffle()
+	cards.print()
 }
